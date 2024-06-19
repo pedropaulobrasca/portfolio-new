33cb50e81b0f66e017b2d5dd08ce3c11815fd6ff
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-06-19 20:28:52 -0300

    chore: Update npm dependencies and add clsx and tailwind-merge packages

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="space-y-4 py-4">
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        <p className="text-sm text-muted-foreground">Hello World</p>
+      </main>
+    </div>
+  )
+}
