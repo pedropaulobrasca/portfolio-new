a9381e8d6a942d53ae82f07b684828c42e7398de
Pedro Brasca <pedropaulobrasca@gmail.com>
2024-06-20 18:34:27 -0300

    feat: Send e-mail

---
+++ src/app/api/mail/send-mail/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest, NextResponse } from 'next/server'
+import nodemailer from 'nodemailer'
+
+interface EmailRequest {
+  subject: string
+  text: string
+  email: string
+  name: string
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const { subject, text, email, name }: EmailRequest = await request.json()
+
+    if (!email || !subject || !text || !name) {
+      return NextResponse.json(
+        { error: 'Email, subject, text and name are required.' },
+        { status: 400 },
+      )
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    })
+
+    const mailOptions = {
+      from: email,
+      to: process.env.EMAIL_USER,
+      subject: `Email from ${name} (${email}): ${subject}`,
+      text,
+    }
+
+    // Envia o email
+    await transporter.sendMail(mailOptions)
+    return NextResponse.json(
+      { message: 'Email sent successfully!' },
+      { status: 200 },
+    )
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      { error: 'Failed to send email.' },
+      { status: 500 },
+    )
+  }
+}
